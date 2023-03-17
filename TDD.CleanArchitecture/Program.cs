0a835d490f87ca693046e004792abf0623d6bbad
KyouShou <j_123321@hotmail.com>
2023-03-17 17:44:41 +0800

    重構

--- TDD.CleanArchitecture/Program.cs
+++ TDD.CleanArchitecture/Program.cs
@@ -1,7 +1,10 @@
+using TDD.CleanArchitecture.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IApplScholarshipService , ApplScholarshipService>();
 
 var app = builder.Build();
 
---
+++ TDD.CleanArchitecture/Service/ApplScholarshipService.cs
@@ -0,0 +1,10 @@
+﻿namespace TDD.CleanArchitecture.Service
+{
+    public class ApplScholarshipService : IApplScholarshipService
+    {
+        public void Apply(ApplicationForm applicationForm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
