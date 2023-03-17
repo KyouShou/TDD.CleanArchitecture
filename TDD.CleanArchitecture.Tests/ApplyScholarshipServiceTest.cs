f8b3f5f6150b3259289adc84ef5ee8af08e74f13
KyouShou <j_123321@hotmail.com>
2023-03-17 23:36:06 +0800

    新增測試：ApplyScholarshipService-無異常-Fail

---
+++ TDD.CleanArchitecture.Tests/ApplyScholarshipServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using TDD.CleanArchitecture.Controllers;
+using TDD.CleanArchitecture.Exceptions;
+using TDD.CleanArchitecture.Service;
+using ContentResult = Microsoft.AspNetCore.Mvc.ContentResult;
+
+namespace TDD.CleanArchitecture.Tests
+{
+    internal class ApplyScholarshipServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TearDown]
+        public void End()
+        {
+        }
+
+        [Test]
+        public void All_Ok()
+        {
+            ApplyScholarshipService applyScholarshipService = new ApplyScholarshipService();
+            ApplicationForm applicationForm = new ApplicationForm(12345L, 98765L);
+
+            applyScholarshipService.Apply(applicationForm);
+        }
+    }
+}
--- TDD.CleanArchitecture/Service/ApplyScholarshipService.cs
+++ TDD.CleanArchitecture/Service/ApplyScholarshipService.cs
@@ -5,6 +5,12 @@
         public void Apply(ApplicationForm applicationForm)
         {
             throw new NotImplementedException();
+
+            //調閱學生資料
+            //調閱獎學金規定的資料
+            //查驗是否符合資格
+            //填寫正式申請書
+            //存檔
         }
     }
 }
